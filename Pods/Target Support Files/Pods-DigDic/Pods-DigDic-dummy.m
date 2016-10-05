d344f6e2a6e27b4be0adcc22b71abcff5fe43496
Ilya Denisov <Pro2005@Ilyas-iMac.local>
2016-10-05 20:52:26 +0300

    Removing all files in .gitignore

--- Pods/Target Support Files/Pods-DigDic/Pods-DigDic-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigDic : NSObject
-@end
-@implementation PodsDummy_Pods_DigDic
-@end
--- Pods/Target Support Files/Pods-DigDicTests/Pods-DigDicTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_DigDicTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DigDicTestsVersionString[];
-
